ee5ff7ad0c9f05c370f3b887cf59ec06175df25d
MOON\moon <acttoze@gmail.com>
2018-05-24 15:54:00 +0900

    카필터 검색 구현

--- src/main/java/com/iindicar/indicar/ListViewItem.java
+++ src/main/java/com/iindicar/indicar/ListViewItem.java
@@ -1,27 +1,45 @@
 package com.iindicar.indicar;
 
-public class ListViewItem {
-    private String tv1 ;
-    private String tv2 ;
-    private String tv3 ;
+import java.util.ArrayList;
 
+public class ListViewItem {
+    private ArrayList<String> tvs;
+    private String tv1;
+    private String tv2;
+    private String tv3;
+
+    public void setArray(String tv1, String tv2, String tv3) {
+        tvs.add(tv1);
+        tvs.add(tv2);
+        tvs.add(tv3);
+    }
+
+    public ArrayList<String> getTvs() {
+        return tvs;
+    }
 
     public void set1(String tv1) {
-        this.tv1=tv1;
+        this.tv1 = tv1;
     }
+
     public void set2(String tv2) {
-        this.tv2=tv2;
+        this.tv2 = tv2;
     }
+
     public void set3(String tv3) {
-        this.tv3=tv3;
+        this.tv3 = tv3;
     }
 
     public String get1() {
-        return this.tv1 ;
-    }public String get2() {
-        return this.tv2 ;
-    }public String get3() {
-        return this.tv3 ;
+        return this.tv1;
+    }
+
+    public String get2() {
+        return this.tv2;
+    }
+
+    public String get3() {
+        return this.tv3;
     }
 }
 
--- src/main/java/com/iindicar/indicar/b2_community/ListViewAdapter.java
+++ src/main/java/com/iindicar/indicar/b2_community/ListViewAdapter.java
@@ -7,20 +7,20 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Filter;
 import android.widget.Filterable;
-import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.iindicar.indicar.BaseActivity;
 import com.iindicar.indicar.ListViewItem;
 import com.iindicar.indicar.R;
 
 import java.util.ArrayList;
 
-public class ListViewAdapter extends BaseAdapter implements Filterable{
+public class ListViewAdapter extends BaseAdapter implements Filterable {
 
 
     // Adapter에 추가된 데이터를 저장하기 위한 ArrayList
-    private ArrayList<ListViewItem> listViewItemList = new ArrayList<ListViewItem>() ;
+    private ArrayList<ListViewItem> listViewItemList = new ArrayList<ListViewItem>();
+    private ArrayList<ListViewItem> filteredItemList = listViewItemList;
+    Filter listFilter;
 
     // ListViewAdapter의 생성자
     public ListViewAdapter() {
@@ -30,7 +30,7 @@ public class ListViewAdapter extends BaseAdapter implements Filterable{
     // Adapter에 사용되는 데이터의 개수를 리턴. : 필수 구현
     @Override
     public int getCount() {
-        return listViewItemList.size() ;
+        return filteredItemList.size();
     }
 
     // position에 위치한 데이터를 화면에 출력하는데 사용될 View를 리턴. : 필수 구현
@@ -46,12 +46,12 @@ public class ListViewAdapter extends BaseAdapter implements Filterable{
         }
 
         // 화면에 표시될 View(Layout이 inflate된)으로부터 위젯에 대한 참조 획득
-        TextView titleTextView1 = (TextView) convertView.findViewById(R.id.text_name1) ;
-        TextView titleTextView2 = (TextView) convertView.findViewById(R.id.text_name2) ;
-        TextView titleTextView3 = (TextView) convertView.findViewById(R.id.text_name3) ;
+        TextView titleTextView1 = (TextView) convertView.findViewById(R.id.text_name1);
+        TextView titleTextView2 = (TextView) convertView.findViewById(R.id.text_name2);
+        TextView titleTextView3 = (TextView) convertView.findViewById(R.id.text_name3);
 
         // Data Set(listViewItemList)에서 position에 위치한 데이터 참조 획득
-        ListViewItem listViewItem = listViewItemList.get(position);
+        ListViewItem listViewItem = filteredItemList.get(position);
 
         // 아이템 내 각 위젯에 데이터 반영
         titleTextView1.setText(listViewItem.get1());
@@ -64,17 +64,17 @@ public class ListViewAdapter extends BaseAdapter implements Filterable{
     // 지정한 위치(position)에 있는 데이터와 관계된 아이템(row)의 ID를 리턴. : 필수 구현
     @Override
     public long getItemId(int position) {
-        return position ;
+        return position;
     }
 
     // 지정한 위치(position)에 있는 데이터 리턴 : 필수 구현
     @Override
     public Object getItem(int position) {
-        return listViewItemList.get(position) ;
+        return filteredItemList.get(position);
     }
 
     // 아이템 데이터 추가를 위한 함수. 개발자가 원하는대로 작성 가능.
-    public void addItem(String tv1,String tv2, String tv3) {
+    public void addItem(String tv1, String tv2, String tv3) {
         ListViewItem item = new ListViewItem();
 
         item.set1(tv1);
@@ -87,6 +87,53 @@ public class ListViewAdapter extends BaseAdapter implements Filterable{
 
     @Override
     public Filter getFilter() {
-        return null;
+        if (listFilter == null) {
+            listFilter = new ListFilter();
+        }
+        return listFilter;
+
     }
+
+    private class ListFilter extends Filter {
+
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            FilterResults results = new FilterResults();
+
+            if (constraint == null || constraint.length() == 0) {
+                results.values = listViewItemList;
+                results.count = listViewItemList.size();
+            } else {
+                ArrayList<ListViewItem> itemList = new ArrayList<ListViewItem>();
+
+                for (ListViewItem item : listViewItemList) {
+                    if (item.get1().toUpperCase().contains(constraint.toString().toUpperCase()) ||
+                            item.get2().toUpperCase().contains(constraint.toString().toUpperCase()) ||
+                            item.get3().toUpperCase().contains(constraint.toString().toUpperCase())) {
+                        itemList.add(item);
+                    }
+                }
+
+                results.values = itemList;
+                results.count = itemList.size();
+            }
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+
+            // update listview by filtered data list.
+            filteredItemList = (ArrayList<ListViewItem>) results.values;
+
+            // notify
+            if (results.count > 0) {
+                notifyDataSetChanged();
+            } else {
+                notifyDataSetInvalidated();
+            }
+        }
+    }
+
+
 }
